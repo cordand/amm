6126ed59ddcf59a47415d71c046f01311a452c9a
John Doe <amm@amm-VirtualBox>
2015-03-11 20:49:56 +0100

    ddaa

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Sensetional gianni"
+            echo "Sensational gianni";
         ?>
     </body>
 </html>
